d510a1c079122d68b5e558471c19b0d2971ca641
vibecastor <vibecastor@gmail.com>
2019-07-02 15:14:38 -0700

    add password hash function

--- lib/medium_graphql_api/accounts/user.ex
+++ lib/medium_graphql_api/accounts/user.ex
@@ -34,6 +34,10 @@ defmodule MediumGraphqlApi.Accounts.User do
     |> hash_password
   end
 
+  def hash_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
   def hash_password(changeset) do
     changeset
   end
