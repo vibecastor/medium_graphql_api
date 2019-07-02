839095b3ac52287b4f00ad617e5a9b55b9e7701f
vibecastor <vibecastor@gmail.com>
2019-07-02 16:37:34 -0700

    add user types to schema

---
+++ lib/medium_graphql_api_web/views/schema.ex
@@ -0,0 +1,15 @@
+defmodule MediumGraphqlApiWeb.Schema do
+  use Absinthe.Schema
+
+  # import Types
+  import_types(MediumGraphqlApiWeb.Schema.Types)
+
+  query do
+  end
+
+  # mutation do
+  # end
+
+  # subscription do
+  # end
+end
